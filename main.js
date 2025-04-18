9d7b49940bce95ff4ca1b3b5d4ede83fb7b9669e
v_lfangtian <v_lfangtian@tencent.com>
2025-04-18 10:39:45 +0800

    窗口大小位置设置

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, ipcMain, dialog,Menu } = require('electron/main')
+const { app, BrowserWindow, ipcMain, dialog, Menu } = require('electron/main')
 // 在你文件顶部导入 Node.js 的 path 模块
 const path = require('node:path')
 
@@ -7,77 +7,49 @@ const createWindow = () => {
   // 新建窗口
   //  让窗口加载了一个界面，这个界面就是用web 技术实现 ，这个界面是运行在渲染进程中的 
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    x:50,
+    y:50, // 设置窗口显示的位置，相对于当前屏幕的左上角
+    // show: false, // 窗口初始化时不显示窗口，而是在加载完成后在显示
+    width: 1000,
+    height: 900,
+    maxHeight:900,
+    maxWidth:1000,
+    minHeight:400,
+    minWidth:500, // 可以通过min max 来设置当前应用窗口的最大和最小窗口尺寸
+    resizable:false, // 是否允许窗口的缩放
+    backgroundColor:'#fff',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+  mainWindow.loadURL('https://github.com')
+  // 解决闪烁问题
+  // mainWindow.once('ready-to-show', () => {
+  //   mainWindow.show()
+  // })
 
   // 让主窗口加载文件 html文件，显示具体内容
   mainWindow.loadFile('index.html')
-mainWindow.webContents.on('did-finish-load',()=>{
-  console.log('333---did-finish-load')
-})
-
-mainWindow.webContents.on('dom-ready',()=>{
-  console.log('22---dom-ready')
-})
-  const menu=Menu.buildFromTemplate([
-    {
-      label: app.name,
-      submenu: [
-        {
-          click: () => mainWindow.webContents.send('update-counter', 1),
-          label: 'Increment'
-        },
-        {
-          click: () => mainWindow.webContents.send('update-counter', -1),
-          label: 'Decrement'
-        }
-      ]
-    }
-  ])
-  Menu.setApplicationMenu(menu)
 
 
-  mainWindow.on('close',()=>{
+
+  mainWindow.on('close', () => {
     console.log('8888----close window')
-    mainWindow=null
+    mainWindow = null
   })
-   // Open the DevTools.
+  // Open the DevTools.
   //  mainWindow.webContents.openDevTools()
 }
 
-async function handleFileOpen(){
-  const { canceled, filePaths } = await dialog.showOpenDialog({})
-  if (!canceled) {
-    return filePaths[0]
-  }
-}
 
 // 控制ready事件
 // ready事件是：当 Electron 完成初始化时，发出一次。
 app.on('ready', () => {
-  // ipcMain.handle('ping',()=>'pong')
 
-  ipcMain.handle('dialog:openFile',handleFileOpen)
-  
-  ipcMain.on('set-title', (event, title) => {
-    const webContents = event.sender
-    const win = BrowserWindow.fromWebContents(webContents)
-    win.setTitle(title)
-  })
-
-  ipcMain.on('counter-value', (_event, value) => {
-    console.log(value) // will print value to Node console
-  })
-console.log('1111---ready')
-  
   createWindow()
   app.on('activate', () => {
     // 在 macOS 系统内, 如果没有已开启的应用窗口
-    // 点击托盘图标时通常会重新创建一个新窗口
+    // 点击托盘图标时通常会重新创建一个新窗口 
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
@@ -91,25 +63,8 @@ app.on('window-all-closed', () => {
 })
 
 
-app.on('before-quit', () => {
-  console.log('555----before-quit')
-})
-
-app.on('will-quit', () => {
-  console.log('6666----will-quit')
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('quit', () => {
-  console.log('7777----quit')
-  if (process.platform !== 'darwin') app.quit()
-})
-
 // 在当前文件中你可以引入所有的主进程代码
 // 也可以拆分成几个文件，然后用 require 导入。
-
-
-
 
 
 // 开启热加载
